c98b980cb3825d72587943ea1cfec11f6741ce9a
monxy747 <monxy.official@gmail.com>
2023-11-15 23:55:04 +0900

    fix : App.js をdefault export からnamed exportに変更

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+
