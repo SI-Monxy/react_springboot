cdaca7f38654404a3e86f4ad2ce59beda66eb585
monxy747 <12smnms20@gmail.com>
2023-11-16 12:11:16 +0900

    add : axiosを利用してapi呼び出し

--- frontend/src/components/Basic1.js
+++ frontend/src/components/Basic1.js
@@ -2,16 +2,16 @@ import React, { useState } from 'react'
 
 const Basic1 = () => {
 
-    const [product, setProduct] = useState({name: '', price: ''});
+    const [product, setProduct] = useState({ name: '', price: '' });
 
     return (
         <>
             <form>
                 <input type="text" value={product.name}
-                onChange={evt => setProduct({...product, name: evt.target.value})}/>
+                    onChange={evt => setProduct({ ...product, name: evt.target.value })} />
 
                 <input type="text" value={product.price}
-                onChange={evt => setProduct({...product, price: evt.target.value})}/>
+                    onChange={evt => setProduct({ ...product, price: evt.target.value })} />
             </form>
             <h3>Product name is {product.name}</h3>
             <h3>Product price is {product.price}</h3>
