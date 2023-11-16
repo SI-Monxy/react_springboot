15f6e529578bd38de54a256094f98087b147c784
monxy747 <12smnms20@gmail.com>
2023-11-16 09:56:16 +0900

    add : useStateの学習　数字、配列、オブジェクト

--- frontend/src/components/Basic1.js
+++ frontend/src/components/Basic1.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Basic1 = (props) => {
-    const clickHandler = () =>{
-        console.log("clicked");
-    }
+const Basic1 = () => {
+
+    const [product, setProduct] = useState({name: '', price: ''});
+
     return (
         <>
-            <button onClick={clickHandler}>Click</button>
-            <h1>Hello {props.name}</h1>
-            <h1>Hello React2</h1>
+            <form>
+                <input type="text" value={product.name}
+                onChange={evt => setProduct({...product, name: evt.target.value})}/>
+
+                <input type="text" value={product.price}
+                onChange={evt => setProduct({...product, price: evt.target.value})}/>
+            </form>
+            <h3>Product name is {product.name}</h3>
+            <h3>Product price is {product.price}</h3>
         </>
     )
 }
